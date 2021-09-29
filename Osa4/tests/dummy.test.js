9b130d20780d458ec715ad96366d8c2e3a26c88b
Tande <taneli.vaananen@gmail.com>
2021-09-29 22:37:07 +0300

    list length tests successful

--- Osa4/tests/dummy.test.js
+++ Osa4/tests/dummy.test.js
@@ -7,10 +7,17 @@ test('dummy returns one', () => {
   expect(result).toBe(1)
 })
 
-test('totalLikes returns 111', () => {
-    const blog = {title:'', author: '', url: '', likes: 0}
+describe('total likes', () => {
+    const blog = {title:'', author: '', url: '', likes: 5}
     const blogs =[{...blog, likes: 1}, {...blog, likes: 10}, {...blog, likes: 100}]
-    const result = listHelper.totalLikes(blogs)
-    expect(result).toBe(111)
+    const result1 = listHelper.totalLikes([])
+    const result2 = listHelper.totalLikes([blog])
+    const result3 = listHelper.totalLikes(blogs)
+    
+    test('of empty list zero', () => expect(result1).toBe(0))
+    test('of length one list is the likes of the element', () => {
+        expect(result2).toBe(5)
+    })
+    test('of bigger calculated right', () => {
+        expect(result3).toBe(111)})
 })
-
