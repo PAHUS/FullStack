303548954bd552dcde9087687ef2b95957912382
Tande <taneli.vaananen@gmail.com>
2021-09-29 20:51:13 +0300

    app refactored - stabile

--- Osa4/index.js
+++ Osa4/index.js
@@ -1,22 +1,11 @@
-const logger = require('./utils/logger')
+const app = require('./app') 
+const http = require('http')
 
 const config = require('./utils/config')
-const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const blogsRouter = require('./controllers/blogs')
-const mongoose = require('mongoose')
+const logger = require('./utils/logger')
 
+const server = http.createServer(app)
 
-mongoose.connect(config.MONGODB_URI)
-
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/blogs', blogsRouter)
-
-
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
