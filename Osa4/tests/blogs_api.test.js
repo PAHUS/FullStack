6815dd4a0636f092518b576cb9823c80953c1e78
Tande <taneli.vaananen@gmail.com>
2021-10-18 21:50:20 +0300

    tests working with test db

--- Osa4/tests/blogs_api.test.js
+++ Osa4/tests/blogs_api.test.js
@@ -3,23 +3,32 @@ const supertest = require('supertest')
 const app = require('../app')
 
 const api = supertest(app)
+const Blog = require('../models/blog')
+const initBlogs = [{title: "a", author: "a", url: '', likes: 12},{title: "b", author: "b", url: '', likes: 21}]
+beforeEach(async () => {
+      await Blog.deleteMany({})
+      let blogObject = new Blog(initBlogs[0])
+      await blogObject.save()  
+      blogObject = new Blog(initBlogs[1])  
+      await blogObject.save()
+})
 
-test('notes are returned as json', async () => {
+test('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
 })
-test('there are two notes', async () => {
+test('all blogs are returned', async () => {
     const response = await api.get('/api/blogs')
   
-    expect(response.body).toHaveLength(2)
+    expect(response.body).toHaveLength(initBlogs.length)
 })
   
-test('the first note is about HTTP methods', async () => {
+test('a specific blog is within the returned blogs', async () => {
     const response = await api.get('/api/blogs')
-
-    expect(response.body[0].title).toBe('Fullstack')
+    const titles = response.body.map(r => r.title)
+    expect(titles).toContainEqual('a')
 })
 
 afterAll(() => {
