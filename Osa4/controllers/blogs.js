6ad30c486c5162d90e096a6cd8b5aa7828e2b9a3
Tande <taneli.vaananen@gmail.com>
2021-10-21 04:45:00 +0300

    deleting works

--- Osa4/controllers/blogs.js
+++ Osa4/controllers/blogs.js
@@ -15,6 +15,17 @@ blogsRouter.post('/', async (request, response) => {
         response.status(400).json({status: 400, message: e.message})
     }
 })
+
+blogsRouter.delete('/:id', async (request, response) => {
+     try {
+        await Blog.findByIdAndDelete(request.params.id)
+        response.status(204).end()
+         
+     } catch (e) {
+         response.status(404).send({error: e.message})
+
+     }
+})
 module.exports = blogsRouter
 
 
