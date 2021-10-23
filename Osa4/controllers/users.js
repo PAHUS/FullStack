10e64f6a5aae9e8b48c5f72edc46064cdcecc91e
Tande <taneli.vaananen@gmail.com>
2021-10-24 01:56:16 +0300

    4.16

--- Osa4/controllers/users.js
+++ Osa4/controllers/users.js
@@ -3,20 +3,26 @@ const usersRouter = require('express').Router()
 const User = require('../models/user')
 
 usersRouter.post('/', async (request, response) => {
-  const body = request.body
+  try{
+    const body = request.body
 
-  const saltRounds = 10
-  const passwordHash = await bcrypt.hash(body.password, saltRounds)
+    const saltRounds = 10
+    if (body.password.length < 3) {
+        throw new ValidationError('pwd length <3')
+    }
+    const passwordHash = await bcrypt.hash(body.password, saltRounds)
 
-  const user = new User({
-    username: body.username,
-    name: body.name,
-    passwordHash,
-  })
+    const user = new User({
+        username: body.username,
+        name: body.name,
+        passwordHash,
+    })
 
-  const savedUser = await user.save()
+    const savedUser = await user.save()
 
-  response.json(savedUser)
+    response.json(savedUser)
+  } catch (e) {
+      response.status(400).json({error:'Invalid username/password format'})}  
 })
 
 
