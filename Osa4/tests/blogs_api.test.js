51e4e3de3238afe10106da8fbcffb7fcffcca1e6
Tande <taneli.vaananen@gmail.com>
2021-10-25 22:54:02 +0300

    part 4 done

--- Osa4/tests/blogs_api.test.js
+++ Osa4/tests/blogs_api.test.js
@@ -6,12 +6,21 @@ const api = supertest(app)
 const Blog = require('../models/blog')
 const User = require('../models/user')
 const initBlogs = [{title: "a", author: "a", url: 'a', likes: 12},{title: "b", author: "b", url: 'b', likes: 21}]
+
+
 beforeEach(async () => {
-      await Blog.deleteMany({})
-      let blogObject = new Blog(initBlogs[0])
-      await blogObject.save()  
-      blogObject = new Blog(initBlogs[1])  
-      await blogObject.save()
+    
+    await User.deleteMany({})
+    //error: 
+    const user = {username: 'testuser', name: 'testname', password: 'testpassword'}
+    const resp = await api.post('/api/users').set('Content-type', 'application/json').send(user)
+    
+    
+    await Blog.deleteMany({})
+    let blogObject = new Blog(initBlogs[0])
+    await blogObject.save()  
+    blogObject = new Blog(initBlogs[1])  
+    await blogObject.save()
 })
 
 test('blogs are returned as json', async () => {
@@ -39,6 +48,7 @@ test('id field is named correctly', async () => {
 })
 
 test('POST adds a blog', async () => {
+    
     const res1 = await api.get('/api/blogs')
     const length1 = res1.body.length
     const blogObject = new Blog({title: "c", author: "c", url: 'c', likes: 3131})
@@ -55,11 +65,21 @@ test('likes is 0 by default', async () => {
 })
 
 test('no title or url gives 400 Bad Request error', async () => {
+    
+    const info = await api.post('/api/login').send({username: 'testuser', password: 'testpassword'})
+    token = info.body.token
     const blogObject = new Blog({author: 'faulty', likes: 2})
+    console.log('')
     await api.post('/api/blogs')
         .send(blogObject)
+        .set(`Authorization`, `bearer ${token}`)
         .expect(400)
-
+})
+test('posting blog without auth sends 401 error', async () => {
+    const blogObject = new Blog({author: 'faulty', title: 'faulty', likes: 2})
+    await api.post('/api/blogs')
+        .send(blogObject)
+        .expect(401)
 })
 
 afterAll(() => {
