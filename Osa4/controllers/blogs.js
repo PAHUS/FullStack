87de4a2edc22d952f8eb1188f4e2508eea948f93
Tande <taneli.vaananen@gmail.com>
2021-10-25 04:21:04 +0300

    refactor to middleware works

--- Osa4/controllers/blogs.js
+++ Osa4/controllers/blogs.js
@@ -18,7 +18,7 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
     const body = request.body
-    const token = getTokenFrom(request)
+    const token = request.token
     const decodedToken = jwt.verify(token, process.env.SECRET)
     if (!token || !decodedToken.id) {
         return response.status(401).json({ error: 'token missing or invalid' })  
