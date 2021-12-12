471b508b771341aa33d3229340832f71b9bc7639
Tande <taneli.vaananen@gmail.com>
2021-12-13 00:56:24 +0200

    login saved to storage

--- Osa5/bloglist-frontend/src/App.js
+++ Osa5/bloglist-frontend/src/App.js
@@ -19,12 +19,30 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])  
+
+
+
+  
+
   const handleLogin = async (event) => {
     event.preventDefault()
     try {
       const user = await loginService.login({
         username, password,
       })
+
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      )
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
