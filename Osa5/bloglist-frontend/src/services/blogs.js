ece82eec09d27ea0eef5bd32c61fdf35893f6688
Tande <taneli.vaananen@gmail.com>
2021-12-18 21:53:58 +0200

    eslint done and mistakes fixed

--- Osa5/bloglist-frontend/src/services/blogs.js
+++ Osa5/bloglist-frontend/src/services/blogs.js
@@ -35,5 +35,5 @@ const update = (id, newObject) => {
   const request = axios.put(`${baseUrl}/${id}`, newObject)
   return request.then(response => response.data)
 }
-
-export default { getAll, create, update, setToken ,remove}
+const exp = { getAll, create, update, setToken ,remove}
+export default exp
